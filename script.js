2679d59b384972628e97436c1d61e7feb583aacf
owaym <owaym123@gmail.com>
2024-02-02 17:54:31 +0530

    Added playRound function to set winner/loser/tie

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-const possibleChoices = ['rock', 'paper', 'scissors'];
+
 
 function getComputerChoice()
 {
+    const possibleChoices = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() * possibleChoices.length);
     const computerChoice = possibleChoices[randomNumber];
     return computerChoice;
 }
-console.log(getComputerChoice(getComputerChoice));
+
+function playRound (playerSelection, computerSelection)
+{
+    if (playerSelection === computerSelection)
+    {
+        console.log ("It's a tie"); 
+    }
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors')||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    )
+    {
+        console.log("You win!");
+    }
+    else{
+        console.log("You lose!");
+    }
+}   
+
+const playerSelection = prompt("Choose rock paper or scissors").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
